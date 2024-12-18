665fc9d9cd42c40218bb363f47ec1b2bdc91f81d
Your Name <you@example.com>
2024-12-18 17:51:49 +0200

    Answer Q1

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Write a program that allows the user to insert an integer then print all numbers between 1 to that number
+            //Console.WriteLine("Enter Your Number To Print All Numbers Between 1 To This Number ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= num; i++) { Console.Write($"{i}, "); }
+
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
