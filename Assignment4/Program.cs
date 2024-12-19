97dde54302d0524c6839a1acf795c6f39cfc231d
Your Name <you@example.com>
2024-12-19 09:05:34 +0200

    Answer Q6

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-            #region Write a program that allows the user to insert an integer then print all numbers between 1 to that number
+            #region 1.Write a program that allows the user to insert an integer then print all numbers between 1 to that number
             //Console.WriteLine("Enter Your Number To Print All Numbers Between 1 To This Number ");
             //int num = Convert.ToInt32(Console.ReadLine());
             //for (int i = 1; i <= num; i++) { Console.Write($"{i}, "); }
             #endregion
 
-            #region Write a program that allows the user to insert an integer then print a multiplication table up to 12
+            #region 2.Write a program that allows the user to insert an integer then print a multiplication table up to 12
 
             //Console.WriteLine("Enter Number to print a multiplication table up to 12:");
             //int num = Convert.ToInt32(Console.ReadLine());
@@ -18,7 +18,7 @@
 
             #endregion
 
-            #region Write a program that allows to user to insert number then print all even numbers between 1 to this number
+            #region 3.Write a program that allows to user to insert number then print all even numbers between 1 to this number
 
             //Console.WriteLine("Enter Number to print print all even numbers between 1 to this number:");
             //int Counter = Convert.ToInt32(Console.ReadLine());
@@ -26,7 +26,7 @@
 
             #endregion
 
-            #region Write a program that takes two integers then prints the power
+            #region 4.Write a program that takes two integers then prints the power
 
             //Console.WriteLine("Enter First Number You Want to Get The power : ");
             //float inputNum = float.Parse(Console.ReadLine());
@@ -38,22 +38,22 @@
 
             #endregion
 
-            #region Write a program to enter marks of five subjects and calculate total, average and percentage
+            #region 5.Write a program to enter marks of five subjects and calculate total, average and percentage
 
             //Console.WriteLine("Enter Marks of five subjects:");
-            
+
             //Console.Write("Subject 1: ");
             //int subject1 = Convert.ToInt32(Console.ReadLine());
 
             //Console.Write("Subject 2: ");
             //int subject2 = Convert.ToInt32(Console.ReadLine());
-            
+
             //Console.Write("Subject 3: ");
             //int subject3 = Convert.ToInt32(Console.ReadLine());
 
             //Console.Write("Subject 4: ");
             //int subject4 = Convert.ToInt32(Console.ReadLine());
-            
+
             //Console.Write("Subject 5: ");
             //int subject5 = Convert.ToInt32(Console.ReadLine());
 
@@ -66,6 +66,42 @@
             //Console.WriteLine($"Percentage = {percentage}%");
 
             #endregion
+
+            #region 6.Write a program to input the month number and print the number of days in that month
+            //Console.WriteLine("Enter Month To Get The Number of Days");
+            //int month = int.Parse(Console.ReadLine());
+            //int days;
+            
+            //switch(month)
+            //{
+            //    case 1:
+            //    case 3:
+            //    case 5:
+            //    case 7:
+            //    case 8:
+            //    case 10:
+            //    case 12:
+            //        days = 31;
+            //        break;
+            //    case 4:
+            //    case 6:
+            //    case 9:
+            //    case 11:
+            //        days = 30;
+            //        break;
+            //    case 2:
+            //        Console.Write("Enter the year to check for leap year: ");
+            //        int year = int.Parse(Console.ReadLine());
+            //        days = (DateTime.IsLeapYear(year)) ? 29 : 28;
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid month number. Please enter a number between 1 and 12.");
+            //        return;
+            //}
+            //Console.WriteLine($"Days in Month: {days}");
+
+            #endregion
+
             Console.ReadKey();
         }
     }
